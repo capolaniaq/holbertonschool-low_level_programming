9813286871787138282c445a654d867b764ebc70
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-02-04 21:51:16 +0000

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	int i;
+
+	for (i = 48; i <= 102; i++)
+	{
+
+	putchar(i);
+
+		if (i == 57)
+		{
+
+		i = 96;
+
+		}
+
+	}
+
+	putchar('\n');
+
+	return (0);
+}
