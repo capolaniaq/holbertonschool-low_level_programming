1330578d47c719a78aad636d70b84817779c7cd0
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-02-04 22:20:04 +0000

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+
+	putchar(i);
+
+	putchar(',');
+
+	putchar('\t');
+
+	}
+
+	putchar('\n');
+
+	return (0);
+}
