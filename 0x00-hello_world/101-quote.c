7e433d80460e82bba5ccadaf33f1d0e905631d5a
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-02-05 16:17:30 +0000

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-
+#include <string.h>
+#include <unistd.h>
 /**
  * main - Entry point
  *
@@ -7,11 +8,8 @@
  */
 int main(void)
 {
-	char m[];
 
-	m = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	scanf("%s", m);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 	return (1);
 }
