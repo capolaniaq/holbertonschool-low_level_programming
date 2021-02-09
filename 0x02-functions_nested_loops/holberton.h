bd1ba31892f7abeeffad90152b52f500cc12adaf
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-02-09 05:45:02 +0000

    holberton.h

---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,25 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+/* _putchar - writes the character c to stdout*/
+int _putchar(char c);
+/* print_alphabet(); print alphabet to lowercase*/
+void print_alphabet(void);
+/* print_alphabet(); print alphabet to lowercase */
+void print_alphabet_x10(void);
+/*int_inslower:wq!*/
+int _islower(int c);
+/*write a funtion that chechs for alphabetic*/
+int _isalpha(int c);
+/*write the funtion that prints the sign of a number*/
+int print_sign(int n);
+/*write a function that computes the absolute value of an integer*/
+int _abs(int);
+/*write a function thath prints the last digit of a number*/
+int print_last_digit(int);
+
+
+
+
+
+#endif
