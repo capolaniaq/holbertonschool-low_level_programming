912eef59abb96f252fb3d8c5c0ccc355c8b50c01
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-02-09 22:53:32 +0000

    holberton.h

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -5,5 +5,5 @@
 /*debugging task positive or negative*/
 void positive_or_negative(int i);
 /*this program prints the largest of three integers*/
-int largest_number(int a, int b, int c);
+int largest_number(int, int, int);
 #endif /* HOLBERTON_H */
