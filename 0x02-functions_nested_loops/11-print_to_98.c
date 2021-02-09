d97567cfddcc339678f84d21029b16f4808d788e
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-02-09 19:27:43 +0000

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * print_to_98 - returns 1 is lowercase, 0 otherwise
+ * @n : input paramether
+ * Return: alwais 0;
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void print_to_98(int n)
+{
+	int a;
+
+	if (n == 98)
+	{
+	printf("%d", n);
+	}
+	else if (n < 98)
+	{
+		for (a = n; a <= 98; a++)
+		{
+		printf("%d, ", a);
+		}
+	}
+	else if (n > 98)
+	{
+		for (a = n; a >= 98; a--)
+		{
+		printf("%d, ", a);
+		}
+
+	}
+	printf("\n");
+}
