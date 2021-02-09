921063ec74ffe6f7ccc0689314e300c9ad5026a3
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-02-09 18:10:32 +0000

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * add - returns add 2 numbres
+ * @x: Input parameter
+ * @y: Input parameter
+ * Return: add two numbers.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
