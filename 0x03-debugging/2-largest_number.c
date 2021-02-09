7edf70c2c146b923619d7d2588b1340f4766cbcb
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-02-09 23:20:13 +0000

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -14,21 +14,15 @@ int largest_number(int a, int b, int c)
 
 	if (a > b && a > c)
 	{
-
 	largest = a;
-
 	}
 	else if (b > a && b > c)
 	{
-
 	largest = b;
-
 	}
-	else
+	else if (c > a && c > b)
 	{
-
 	largest = c;
-
 	}
 
 	return (largest);
