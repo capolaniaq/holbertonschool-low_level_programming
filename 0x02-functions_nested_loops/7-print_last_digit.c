23a6fb7fa897b188596aa22fa62942287f52ffae
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-02-09 06:31:13 +0000

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -13,25 +13,6 @@ int print_last_digit(int x)
 	if (x < 0)
 		x = x * (-1);
 
-	if (x == 0)
-		_putchar(48);
-	else if (x == 1)
-		_putchar(49);
-	else if (x == 2)
-		_putchar(50);
-	else if (x == 3)
-		_putchar(51);
-	else if (x == 4)
-		_putchar(52);
-        else if (x == 5)
-		_putchar(53);
-	else if (x == 6)
-		_putchar(54);
-	else if (x == 7)
-		_putchar(55);
-	else if (x == 8)
-		_putchar(56);
-	else if (x == 9)
-		_putchar(57);
+	_putchar('0' + x);
 	return (x);
 }
