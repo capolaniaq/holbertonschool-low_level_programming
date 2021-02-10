63fc61045f9b87c6eff098effe717a848db55233
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-02-10 02:07:41 +0000

    holberton.h

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -2,15 +2,24 @@
 #define HOLBERTON_H
 
 #include <stdio.h>
-/*debugging task positive or negative*/
+
 void positive_or_negative(int i);
-/*this program prints the largest of three integers*/
-int largest_number(int a, int b, int c);
-/*convert_day - converts day of month to day of year, without accounting for leap year */
-int convert_day(int month, int day);
-/*  * print_remaining_days - takes a date and prints how many days are
-* left in the year, taking leap years into account */
-void print_remaining_days(int month, int day, int year);
-
-
 #endif /* HOLBERTON_H */
+
+#ifndef LARGEST_NUMBER_H
+#define LARGEST_NUMBER_H
+
+int largest_number(int a, int b, int c);
+#endif
+
+#ifndef CONVERT_DAY_H
+#define CONVERT_DAY_H
+
+int convert_day(int month, int day);
+#endif
+
+#ifndef PRINT_REMAINING_DAYS_H
+#define PRINT_REMAINING_DAYS_H
+
+void print_remaining_days(int month, int day, int year);
+#endif
