f25e9308a6a56ddfff1e74461abad9ec2811e601
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-02-11 20:59:25 +0000

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+* main - not parameters inside
+* Return - 0 alwais
+*
+*/
+
+int main (void)
+{
+	int a;
+
+	for (a =1; a <= 100; a++)
+	{
+
+		if (a % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (a % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else if (a % 3 == 0 && a % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else 
+		{
+			printf("%d", a);
+		}
+		printf("\t");
+
+	} 
+	
+	printf("\n");
+	return (0); 
+
+}
