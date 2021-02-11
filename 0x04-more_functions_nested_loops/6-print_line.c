7139485b2ac9a482b135c4cf49dc2a1e81c51adb
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-02-11 18:02:56 +0000

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - returns 1 is lowercase, 0 otherwise
+ * @n - don't have parameters inside
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+void print_line(int n)
+{
+	int a;
+
+	for (a = 1; a <= n; a++)
+	{
+		_putchar(95);
+	
+	}
+
+	_putchar(10);
+}
