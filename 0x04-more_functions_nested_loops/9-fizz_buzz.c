b98310604f2faab64fd327eea37dc0fa618afa32
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-02-11 22:45:48 +0000

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,15 +15,18 @@ int main(void)
 
 		if (a % 3 == 0 && a % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
+			printf(" ");
 		}
 		else if (a % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
+			printf(" ");
 		}
 		else if (a % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
+			printf(" ");
 		}
 		else if (a == 100)
 		{
@@ -32,6 +35,7 @@ int main(void)
 		else
 		{
 			printf("%d ", a);
+			printf(" ");
 		}
 
 	}
