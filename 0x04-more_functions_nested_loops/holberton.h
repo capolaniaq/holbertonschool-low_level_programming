867fe2b9cb72ef56c81663ed298cf78793822c02
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-02-11 17:12:12 +0000

    holberton.h README.md

---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,31 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+/*_putchar -writes the character c to stdout*/
+int _putchar (char c);
+/*_isupper - display if the character c is upper or no*/
+int _isupper(int c);
+/*isdigit - display if the character is digit ot no*/
+int _isdigit(int c);
+/*mul - display multiplication among two integers*/
+int mul(int a, int b);
+/**/
+void print_numbers(void);
+/**/
+void print_most_numbers(void);
+/**/
+void more_numbers(void);
+/**/
+void print_line(int n);
+/**/
+void print_diagonal(int n);
+/**/
+void print_square(int size);
+/**/
+void print_triangle(int size);
+/**/
+
+
+
+
+
+#endif
