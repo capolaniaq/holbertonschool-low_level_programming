454045184863919e8da7e6744c25568513f054c8
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-02-16 19:06:39 +0000

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+* puts_half - reset of the n a 98
+* @str: char of the before function
+*
+* Return: always 0
+*/
+
+void puts_half(char *str)
+{
+	int a = 0;
+
+	int c;
+
+	while (*(str + a) != '\0')
+		a++;
+
+	for (c = a / 2; c <= a - 1; c++)
+	{
+		_putchar(str[c]);
+	}
+
+	_putchar(10);
+
+}
