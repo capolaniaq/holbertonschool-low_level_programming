059edc17ef77ce23480488d999b6ac6af5f5afe0
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-02-16 18:25:26 +0000

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+* puts2 - reset of the n a 98
+* @str: char of the before function
+*
+* Return: always 0
+*/
+
+void puts2(char *str)
+{
+	int a = 0;
+
+	int c;
+
+	while (*(str + a) != '\0')
+		a++;
+
+	for (c = 0; c < a - 1; c++)
+	{
+		if (str[c] % 2 == 0)
+		{
+			_putchar(str[c]);
+		}
+	}
+
+	_putchar(10);
+
+}
