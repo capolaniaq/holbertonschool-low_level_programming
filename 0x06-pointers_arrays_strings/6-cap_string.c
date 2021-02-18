7687ac7458de36724dafc099e27dcec6fc3600d3
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-02-18 23:53:50 +0000

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,32 +1,38 @@
 #include "holberton.h"
-
 /**
 * *cap_string - capitallising the string
 * @s: char for the capitalising
 * Return: char whit the capitalized
 */
-
 char *cap_string(char *s)
 {
 	int i, j;
 
 	i = 0;
-
 	while (*(s + i) != '\0')
 		i++;
-
 	for (j = 0; j <= i; j++)
 	{
 		if (s[j] == '\n')
+		{
 			s[j] = 10;
-		else if (s[j] == '\t')
+			s[j + 1] = s[j + 1] - 32;
+		}
+		else if (s[j] == '\t' || s[j] == 32)
+		{
 			s[j] = 32;
+			if (!(s[j + 1] >= 65 && s[j + 1] <= 90) && !(s[j + 1] >= 48 && s[j + 1] <= 57))
+				s[j + 1] = s[j + 1] - 32;
+		}
 		else if (s[j] == ',')
 			s[j] = 44;
 		else if (s[j] == ';')
 			s[j] = 59;
 		else if (s[j] == '.')
-			s[j] = 46;
+		{
+			if (s[j + 1]  >= 97 && s[j + 1] <= 122)
+				s[j + 1] = s[j + 1] - 32;
+		}
 		else if (s[j] == '!')
 			s[j] = 33;
 		else if (s[j] == '?')
@@ -43,6 +49,4 @@ char *cap_string(char *s)
 			s[j] = 125;
 	}
 	return (s);
-
-
 }
