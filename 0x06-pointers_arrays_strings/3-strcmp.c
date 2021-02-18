fece7720bf44c67b21970aa75c4e7ce8f308fd6f
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-02-18 20:30:17 +0000

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,9 +1,10 @@
 #include <string.h>
 
 /**
-*
-*
-*
+* _strcmp - compare two strings
+* @s1: string one
+* @s2: string two
+* Return: value of less caracters in ascii
 */
 
 int _strcmp(char *s1, char *s2)
@@ -15,7 +16,5 @@ int _strcmp(char *s1, char *s2)
 	if (s1[a] != s2[a])
 		a = s1[a] - s2[a];
 
-	return(a);
-
-
+	return (a);
 }
