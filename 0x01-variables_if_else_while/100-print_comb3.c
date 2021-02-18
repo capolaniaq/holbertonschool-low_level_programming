1264c4689824f01f22b85829e35954d44d8eccfd
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-02-18 00:18:02 +0000

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i, a, b;
+
+	for (i = 0; i <= 99; i++)
+	{
+		a = i / 10;
+		b = i % 10;
+
+		if (a != b)
+		{
+			if (i < 10)
+			{
+				putchar('0' + a);
+				putchar('0' + b);
+				putchar(44);
+				putchar(32);
+			}
+			else if (i % 10 == 0)
+			{
+				a++;
+			}
+			else if (a >= b)
+			{
+				a++;
+			}
+			else
+			{
+				putchar('0' + a);
+				putchar('0' + b);
+					if (i != 89)
+					{
+						putchar(44);
+						putchar(32);
+					}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
