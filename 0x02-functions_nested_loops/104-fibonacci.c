a161904e83703aed38b95292c9c359589d4d01ea
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-02-21 19:23:53 +0000

    final final

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+* main - funcion firts 50 fibonacci
+* void - no value into
+* Return: always zero
+*/
+
+int main(void)
+{
+	int i;
+	unsigned long int j, a, b, k;
+
+	j = 0;
+	a = 0;
+	b = 1;
+	k = 0;
+
+	for (i = 0; i < 98; i++)
+	{
+		j = a + b;
+		a = b;
+		b = j;
+		printf("%lu", j);
+		if (i != 97)
+			printf(", ");
+
+
+	}
+
+	printf("\n");
+	return (0);
+
+}
