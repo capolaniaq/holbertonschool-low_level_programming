feb1a4b3b8591edb53e53f1f32b427f898b08cde
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-02-21 04:49:56 +0000

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,40 +9,41 @@
 
 void print_times_table(int n)
 {
-
 	int a, i, j, k;
-
-	for (i = 0; i <= n; i++)
+	if (n <= 15 && n > 0)
 	{
-		for (j = 0; j <= n; j++)
+		for (i = 0; i <= n; i++)
 		{
-			k = i * j;
-			if (j == 0)
-				_putchar('0' + k);
+			for (j = 0; j <= n; j++)
+			{
+				k = i * j;
+				if (j == 0)
+					_putchar('0' + k);
 
-			_putchar(32);
-			if (k < 10)
-			{
 				_putchar(32);
-				_putchar(32);
-				_putchar('0' + k);
+				if (k < 10)
+				{
+					_putchar(32);
+					_putchar(32);
+					_putchar('0' + k);
+				}
+				else if (k >= 10 && k < 100)
+				{
+					_putchar(32);
+					_putchar('0' + k / 10);
+					_putchar('0' + k % 10);
+				}
+				else if (k >= 100 && k < 1000)
+				{
+					a = k / 10;
+					_putchar('0' + a / 10);
+					_putchar('0' + a % 10);
+					_putchar('0' + k % 10);
+				}
+				if (j != n)
+					_putchar(44);
 			}
-			else if (k >= 10 && k < 100)
-			{
-				_putchar(32);
-				_putchar('0' + k / 10);
-				_putchar('0' + k % 10);
-			}
-			else if (k >= 100 && k < 1000)
-			{
-				a = k / 10;
-				_putchar('0' + a / 10);
-				_putchar('0' + a % 10);
-				_putchar('0' + k % 10);
-			}
-			if (j != n)
-				_putchar(44);
+			_putchar(10);
 		}
-		_putchar(10);
 	}
 }
