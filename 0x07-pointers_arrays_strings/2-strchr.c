db297e76124de38ef1dc6e03d18afe95994ffa25
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-02-22 20:27:39 +0000

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,10 +1,10 @@
 #include "holberton.h"
 
 /**
-*
-*
-*
-*
+* *_strchr - function that locates a character in a string
+* @s: string
+* @c: ocurrence character
+* Return: Null if the character not found or string ocurrence
 */
 
 char *_strchr(char *s, char c)
@@ -14,7 +14,7 @@ char *_strchr(char *s, char c)
 	for (i = 0; s[i] != '\0'; i++, s++)
 	{
 		if (*s == c)
-			return(s);
+			return (s);
 	}
 
 	return (0);
