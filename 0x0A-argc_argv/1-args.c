791cde17a072ee4df1087f275d4de2113b0570db
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-02-28 02:58:24 +0000

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+*
+*
+*
+*
+*/
+
+
+int main(int argc, char *argv[])
+{
+	if (argc != 0 && argv != '\0')
+	{
+	printf("%d\n", argc);
+	}
+
+	return (0);
+}
