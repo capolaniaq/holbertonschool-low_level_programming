9561e8e59c926d4bb46956b27f10f5314d95b0cb
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-02-28 03:02:25 +0000

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,19 +2,20 @@
 #include <stdio.h>
 
 /**
-*
-*
-*
-*
+* main - print argcñ
+* @argc: numbers for the characters
+* @argv: char for the compare
+* Return: always 0
 */
-
 
 int main(int argc, char *argv[])
 {
 	if (argc != 0 && argv != '\0')
 	{
-	printf("%d\n", argc);
+	printf("%d", argc);
 	}
+
+	printf("\n");
 
 	return (0);
 }
