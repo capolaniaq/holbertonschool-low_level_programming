fc7b8ef74e66f3a36a0a20cf23d6799a900b1de9
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-02-28 03:52:06 +0000

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - print the name of the file
+* @argc: number for the caracther argv
+* @argv: character for the print
+* Return: Always 0
+*/
+
+
+int main(int argc, char *argv[])
+{
+	while (argc --)
+		printf("%s\n", *argv++);
+
+	return (0);
+}
