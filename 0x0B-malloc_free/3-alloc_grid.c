965bc4e1072dd9bc82ab944ecfeeed0ea8e3f79a
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-03-03 04:51:01 +0000

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -28,7 +28,13 @@ int **alloc_grid(int width, int height)
 		grid[i] = malloc(width * sizeof(int));
 
 		if (grid[i] == NULL)
+		{
+			for (; i >= 0; i--)
+				free(grid[i]);
+
+			free(grid);
 			return (0);
+		}
 
 		for (j = 0; j < width; j++)
 		{
