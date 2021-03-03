8cdf5cfe71a9cade85de3e3e3ebaa366a27b9f23
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-03-03 04:11:59 +0000

    3-alloc_grid.c

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* **alloc_grid - function that returns a pointer
+* to a 2 dimensional array of integers.
+* @width: width of the matix
+* @height: height of the matix
+* Return: pointer with 2 dimensions of the matix
+*/
+
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **grid;
+
+	grid = malloc(height * sizeof(int *));
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(width * sizeof(int));
+		for (j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
+		}
+	}
+
+	return (grid);
+}
