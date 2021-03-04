161ef7da8d7ddcc6d1ac38f2a9ee11ab92da161b
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-03-04 19:29:00 +0000

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,21 +13,22 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *charconcat;
+	char *null = "";
 	unsigned int lenght1, count1, count2, lenght2;
 
 	lenght1 = 0;
 	count2 = 0;
 	lenght2 = 0;
 	if (s1 ==  NULL)
-		s1 = "";
+		s1 = null;
 	else if (s2 == NULL)
-		s2 = "";
+		s2 = null;
 
 	while (*(s1 + lenght1) != '\0')
 		lenght1++;
 	while (*(s2 + lenght2) != '\0')
 		lenght2++;
-	if (lenght2 >= n)
+	if (lenght2 <= n)
 		lenght2 = n;
 
 	charconcat = malloc((lenght1 + lenght2 + 1) * sizeof(char));
