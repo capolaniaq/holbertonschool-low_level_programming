6a0321b6c46f1611477d4cfed83ccd965508028e
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-03-04 22:31:54 +0000

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 /**
 * *_calloc - function that allocates memory for an array,
 * @nmemb: number of the elements
@@ -17,12 +17,12 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	p = 0;
-
 	p = malloc(size * nmemb);
 
 	if (p == NULL)
 		return (NULL);
+
+	memset(p, 0, (nmemb * size));
 
 	for (i = 0; i < size; i++)
 	{
