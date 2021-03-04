2eba60d520c48e7f8ef39685ec4b5547c4a8846c
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-03-04 19:13:38 +0000

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -28,7 +28,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	while (*(s2 + lenght2) != '\0')
 		lenght2++;
 	if (lenght2 <= n)
-		n = lenght2;
+		lenght2 = n;
 
 	charconcat = malloc((lenght1 + n + 1) * sizeof(char));
 
