81bea78c97e13ef7f58569065d615d94e8a99bb3
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-03-08 23:45:12 +0000

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef _FUNCTIONS_
+#define _FUNCTIONS_
+
+/**
+* struct dog - description my dog
+* @name: name dog char pointer
+* @age: float
+* @owner: char pointer
+*
+*
+* Description: is the features for a do
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+
+
+#endif
