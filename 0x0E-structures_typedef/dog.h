63abe43d431af852892526a92f7b4bf746fa560d
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-03-09 03:50:33 +0000

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -25,4 +25,12 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 /*function that prints a struct dog*/
 void print_dog(struct dog *d);
 
+/* function that creates a new dog*/
+dog_t *new_dog(char *name, float age, char *owner);
+
+/*function that frees dogs*/
+void free_dog(dog_t *d);
+
+
+
 #endif
