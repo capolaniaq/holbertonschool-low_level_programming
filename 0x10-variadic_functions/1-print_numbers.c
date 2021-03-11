0c5cfe1ef59f27ba5c11d965a58473833f962a5e
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-03-11 23:34:37 +0000

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -20,15 +20,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	if (n != 0)
 	{
-		if (separator != 0)
+		for (i = 0; i < n; i++)
 		{
-			for (i = 0; i < n; i++)
-			{
-				printf("%d", va_arg(arguments, int));
+			printf("%d", va_arg(arguments, int));
 
-				if (i != (n - 1))
-					printf("%s", separator);
-			}
+			if (separator != 0 && i != (n - 1))
+				printf("%s", separator);
 		}
 		putchar(10);
 	}
