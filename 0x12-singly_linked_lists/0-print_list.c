c40ed79e997243d1036b5e4c52973b5d8eb4b906
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-03-21 06:13:31 +0000

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -5,7 +5,7 @@
 
 /**
 * print_list - function that prints all the elements of a list_t list
-	* @h: pointer a head of the list
+* @h: pointer a head of the list
 *
 * Return: how many nodes
 */
@@ -15,19 +15,19 @@ size_t print_list(const list_t *h)
 	const list_t *temporary = h;
 	unsigned int i = 0;
 
-	if (!temporary)
+	if (temporary == NULL)
 		return (0);
 
 	while (temporary != NULL)
 	{
 		if (temporary->str == NULL)
-			printf("[0] (nil)");
+			printf("[0] (nil)\n");
 		else
 		{
 		printf("[%d] %s\n", temporary->len, temporary->str);
-		temporary = temporary->next;
 		}
 
+		temporary = temporary->next;
 		i++;
 	}
 
