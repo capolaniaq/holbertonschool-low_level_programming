f0808ba9af6a6871334febd2e0c04d3b770b826f
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-03-21 05:48:45 +0000

    0-print_list.c README.md lists.h

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+* print_list - function that prints all the elements of a list_t list
+	* @h: pointer a head of the list
+*
+* Return: how many nodes
+*/
+
+size_t print_list(const list_t *h)
+{
+	const list_t *temporary = h;
+	unsigned int i = 0;
+
+	if (!temporary)
+		return (0);
+
+	while (temporary != NULL)
+	{
+		if (temporary->str == NULL)
+			printf("[0] (nil)");
+		else
+		{
+		printf("[%d] %s\n", temporary->len, temporary->str);
+		temporary = temporary->next;
+		}
+
+		i++;
+	}
+
+	return (i);
+}
---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,36 @@
+#ifndef _LIST_H_
+#define _LIST_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct list 
+{
+	char *str;
+	unsigned int len;
+	struct list *next;
+};
+
+typedef struct list list_t;
+
+
+/*function that print the character*/
+int _putchar (char c);
+
+/*function that prints all the elements of a list_t list.*/
+size_t print_list(const list_t *h);
+
+/*function that returns the number of elements in a linked list_t list. */
+size_t list_len(const list_t *h);
+
+/* function that adds a new node at the beginning of a list_t list.*/
+list_t *add_node(list_t **head, const char *str);
+
+/*function that adds a new node at the end of a list_t list. */
+list_t *add_node_end(list_t **head, const char *str);
+
+/*function that frees a list_t list. */
+void free_list(list_t *head);
+
+
+#endif /*LIST_H_*/
