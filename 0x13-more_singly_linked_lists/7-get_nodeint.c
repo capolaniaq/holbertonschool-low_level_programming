eb16fbd413e48da939b94e040ce09fc44c022e98
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-03-23 19:44:38 +0000

    7-get_nodeint.c

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+* *get_nodeint_at_index -  function ntn node of list.
+* @head: pointer that first node
+* @index: number of node
+*
+* Return: node of ntn
+*/
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *new = NULL;
+	listint_t *aux = NULL;
+	unsigned int i;
+
+	if (head == NULL)
+		return (0);
+
+	new = head;
+	for (i = 0; i <= index; i++)
+	{
+		if (i == index)
+			aux = new;
+		new = new->next;
+	}
+
+	return (aux);
+}
