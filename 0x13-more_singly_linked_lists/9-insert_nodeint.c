afc7fb665f4434bc0498d00c246cc49666e0c09f
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-03-24 02:09:39 +0000

    9-insert_nodeint.c final final

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,16 +22,17 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 
 	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
 	new->n = n;
 	tmp = *head;
-
 	if (idx == 0)
 	{
 		new->next = *head;
 		*head = new;
+		return (new);
 	}
-
-	for (i = 0; i < idx; i++)
+	for (i = 0; i <= idx; i++)
 	{
 		if (tmp == NULL && i < idx)
 		{
@@ -47,7 +48,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		if (tmp)
 			tmp = tmp->next;
 	}
-
 	return (new);
-
 }
