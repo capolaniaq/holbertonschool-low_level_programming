54df593c394a6ac700aaba878f0b107eac31f9af
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-03-26 17:38:42 +0000

    README.md holberton.h

---
+++ 0x14-bit_manipulation/holberton.h
@@ -0,0 +1,24 @@
+#ifndef _HOLBERTON_H_
+#define -HOLBERTON_H_
+
+/*function that converts a binary number to an unsigned int*/
+unsigned int binary_to_uint(const char *b);
+
+/*function that prints the binary representation of a number.*/
+void print_binary(unsigned long int n);
+
+/*function that returns the value of a bit at a given index.*/
+int get_bit(unsigned long int n, unsigned int index);
+
+/*function that sets the value of a bit to 1 at a given index */
+int set_bit(unsigned long int *n, unsigned int index);
+
+/*function that sets the value of a bit to 0 at a given index*/
+int clear_bit(unsigned long int *n, unsigned int index);
+
+/*function that returns the number of bits you would need to flip*/
+/*to get from one number to another.*/
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+
+
+#endif /*_HOLBERTON_H_*/
