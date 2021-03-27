2315cafb476b9ffc51c4bf0db8c9fbcb1f91a931
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-03-27 00:00:57 +0000

    add functions of the first task in  holberton.h

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -1,8 +1,17 @@
 #ifndef _HOLBERTON_H_
-#define -HOLBERTON_H_
+#define _HOLBERTON_H_
 
 /*function that converts a binary number to an unsigned int*/
 unsigned int binary_to_uint(const char *b);
+
+/***/
+int potencia(int i);
+
+/**/
+unsigned int strlentotal(const char *b);
+
+/**/
+unsigned int strlenfunctional(const char *b);
 
 /*function that prints the binary representation of a number.*/
 void print_binary(unsigned long int n);
