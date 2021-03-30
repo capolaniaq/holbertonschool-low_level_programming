84a765aa378ca956b6b5eda9b9b934bbfdde8693
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-03-30 16:12:14 +0000

    README.md holberton.h

---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,17 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+#define BUFSIZE 1204
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+int _putchar(char c);
+
+
+
+
+#endif
