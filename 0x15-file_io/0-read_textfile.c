388c1b51e9985e265d566c34fabb66c0fcea24c5
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-03-30 16:10:57 +0000

    0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,49 @@
+#include "holberton.h"
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+/**
+* read_textfile - print the content of the file
+* @filename: file to open
+* @letters: letter how many characters to print
+*
+* Return: ssize_t
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file, lprint, lread, closed;
+	char *buffer;
+
+	file = 0;
+	lprint = 0;
+	lread = 0;
+	buffer = malloc(letters * sizeof(char));
+
+	if (!filename)
+		return (0);
+
+	file = open(filename, O_RDONLY);
+
+	if (file == -1)
+		return (0);
+
+	lread = read(file, buffer, letters);
+
+	if (!lread)
+		return (0);
+
+	lprint = write(STDOUT_FILENO, buffer, lread);
+
+	if (lprint == -1)
+		return (0);
+
+	closed = close(file);
+
+	if (closed == -1)
+		return (0);
+
+	free(buffer);
+	return (lread);
+}
