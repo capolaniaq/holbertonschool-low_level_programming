2dd9ab06bed18ea0e098f8fafa51d98bf9e4e0bf
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-03-31 03:19:15 +0000

    2-append_text_to_file.c

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+/**
+* append_text_to_file - write in end of the file
+* @filename: file to open
+* @text_content: content respect in the new file
+*
+* Return: 1 on success, or -1 on failure
+*/
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int file = 0;
+	int bytes_written = 0;
+	int i = 0;
+
+	if (!filename)
+		return (-1);
+	if (!text_content)
+		return (-1);
+	file = open(filename, O_WRONLY | O_APPEND);
+
+	if (file == -1)
+		return (-1);
+
+	while (*(text_content + i) != '\0')
+		i++;
+
+	bytes_written = write(file, text_content, i);
+
+	if (bytes_written == -1)
+		return (-1);
+	return (1);
+}
