8cbdde9d2f35c351299b600f31df16b1bad7c651
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-03-31 03:42:28 +0000

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,11 +21,14 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	file_add = open(filename, O_WRONLY | O_APPEND);
 
-	if (file_add < 0)
+	if (file_add == -1)
 		return (-1);
 
-	while (*(text_content + i) != '\0')
-		i++;
+	if (text_content)
+	{
+		while (text_content[i])
+			i++;
+	}
 
 	write(file_add, text_content, i);
 
