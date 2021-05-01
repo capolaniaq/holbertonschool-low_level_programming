990bcf16105ac9acdf31ce2b0e045100e992264d
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-05-01 22:25:55 +0000

    7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -34,9 +34,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		new_node->next = tmp1, *h = new_node;
 		return (new_node);
 	}
-	while (tmp1 != NULL && i <= idx)
+	while (tmp1 != NULL && i < idx)
 	{
-		if (i == idx)
+		if (i == idx - 1)
 		{
 			tmp2 = tmp1->next;
 			tmp1->next = new_node;
