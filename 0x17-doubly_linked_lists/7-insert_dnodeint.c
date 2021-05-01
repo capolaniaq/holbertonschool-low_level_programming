b21cef747aa6416cfb64ca1837d6a6716282ca64
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-05-01 22:12:02 +0000

    7-insert_dnodeint.c, insert add_dnodeint(h, n) in case that idx == 0;

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -30,6 +30,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 	{
 		new_node = add_dnodeint(h, n);
+		*h = new_node;
 		return (new_node);
 	}
 	while (tmp1 != NULL && i <= idx)
