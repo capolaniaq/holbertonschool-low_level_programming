b5ccd49f41415e3217e5be9183b89441f22ef51e
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-05-28 00:29:04 -0500

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,6 +11,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_hash_table;
 
+	if (size == 0)
+		return (NULL);
 	new_hash_table = malloc((size) * sizeof(hash_table_t));
 
 	if (new_hash_table == NULL)
