2fd022209f9d2d7ee60e2dae45bb514770056132
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-05-28 00:46:17 -0500

    fixing the array size

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,12 +11,17 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_hash_table;
 
-	if (size == 0)
-		return (NULL);
-	new_hash_table = malloc((size) * sizeof(hash_table_t));
+	new_hash_table = malloc(sizeof(hash_table_t));
 
 	if (new_hash_table == NULL)
 		return (NULL);
 
+	new_hash_table->size = size;
+	new_hash_table->array = calloc(size, sizeof(hash_node_t *));
+	if (new_hash_table->array == NULL)
+	{
+		free(new_hash_table);
+		return (NULL);
+	}
 	return (new_hash_table);
 }
