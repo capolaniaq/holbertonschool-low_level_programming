7918a4df35672910d6857e250af820823b35aa72
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-05-27 21:40:24 -0500

    hash_tables.h, 0-hash_table.c

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,22 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - function that create a hash_table
+ * @size: unsigned long int to indicated the size of the array
+ *
+ * Return: hash_table of the size indicated, or NULL in case failure
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+hash_table_t *new_hash_table;
+
+new_hash_table = malloc((size) * sizeof(hash_table_t));
+
+if (new_hash_table == NULL)
+{
+return (NULL);
+}
+
+return (new_hash_table);
+}
