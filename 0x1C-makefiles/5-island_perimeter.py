f76d6f5b3263405403938d3653428ea7457b9c53
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-07-04 12:17:38 -0500

    fixing the size error

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -9,8 +9,7 @@ def island_perimeter(grid):
     colum = []
     grid_change = []
     counter = 0
-    leght = len(grid[0])
-    while (len(grid_change) < leght):
+    while (len(grid_change) < len(grid[0])):
         for row in grid:
             colum.append(row[counter])
         grid_change.append(colum)
