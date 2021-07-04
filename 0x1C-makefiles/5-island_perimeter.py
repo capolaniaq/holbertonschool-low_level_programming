79954c5a7e5647a99ac80e12489111394966667e
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-07-04 02:45:05 -0500

    5-island_perimeter.py

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Module that returns the perimeter of the island described in grid"""
+
+
+def island_perimeter(grid):
+    """perimeter of the island described in grid"""
+    vertically = 0
+    horizontally = 0
+    colum = []
+    grid_change = []
+    counter = 0
+    while (len(grid_change) < len(grid)):
+        for row in grid:
+            colum.append(row[counter])
+        grid_change.append(colum)
+        colum = []
+        counter += 1
+
+    for row in grid:
+        if 1 in row:
+            vertically += 1
+
+    for colum in grid_change:
+        if 1 in colum:
+            horizontally += 1
+
+    return (2*(horizontally + vertically))
