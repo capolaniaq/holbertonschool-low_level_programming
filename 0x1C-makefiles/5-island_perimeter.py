63417ad4b5efe21fa4170d1dc036f749e51ef9f8
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-07-05 11:28:38 -0500

    fixing the error, no solution the problem

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -5,11 +5,26 @@
 def island_perimeter(grid):
     """perimeter of the island described in grid"""
     perimeter = 0
-    for row in grid:
-        for land in row:
+    for i, row in enumerate(grid):
+        for j, land in enumerate(row):
             if land == 1:
-                if perimeter == 0:
-                    perimeter +=3
+                if i == 0:
+                    perimeter += 1
+                elif grid[i - 1][j] == 0:
+                    perimeter += 1
+                if i < len(grid) - 1:
+                    if grid[i + 1][j] == 0:
+                        perimeter += 1
                 else:
-                    perimeter +=2
-    return perimeter + 1
+                    perimeter += 1
+                if j != 0:
+                    if grid[i][j - 1] == 0:
+                        perimeter += 1
+                else:
+                    perimeter += 1
+                if j < len(row) - 1:
+                    if grid[i][j + 1] == 0:
+                        perimeter += 1
+                else:
+                    perimeter += 1
+    return perimeter
