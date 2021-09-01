1fcba14a16246ff0f7c2847f3575601cd9d052bb
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-09-01 01:58:24 -0500

    search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 
 int linear_search(int *array, size_t size, int value);
-
+int binary_search(int *array, size_t size, int value);
+void print_array(int *array, size_t low, size_t size);
+int binarySearch(int *arr, size_t l, size_t r, int x);
 
 #endif
