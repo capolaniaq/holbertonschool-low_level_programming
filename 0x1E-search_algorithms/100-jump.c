ae31d1772881618ce89be599b6768e1c6f8a79e3
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-09-01 15:40:53 -0500

    100-jump.c

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -14,27 +14,34 @@ int jump_search(int *array, size_t size, int value)
 {
 	size_t root;
 	size_t index;
+	size_t idx_max;
 
 	root = sqrt(size);
 
-
-	for (index = 0; index < size; index = index + root)
+	for (index = 0; index <= size; index = index + root)
+	{
+		if (value <= array[index])
+		{
+			break;
+		}
+		printf("Value checked array[%ld] = [%d]\n", index, array[index]);
+	}
+	idx_max = index;
+	if (idx_max >= size)
+	{
+		idx_max = size - 1;
+	}
+	index = index - root;
+	printf("Value found between indexes [%ld] and [%ld]\n",
+		   index, index + root);
+	for (; index <= idx_max; index++)
 	{
 		printf("Value checked array[%ld] = [%d]\n", index, array[index]);
-		if (value >= array[index] && value <= array[index + root])
+		if (array[index] == value)
 		{
-			printf("Value found between indexes [%d] and [%d]\n",
-			array[index], array[index + root]);
-			for (; index < index + root; index++)
-			{
-				printf("Value checked array[%ld] = [%d]\n", index, array[index]);
-				if (array[index] == value)
-				{
-					return (index);
-				}
-			}
+			return (index);
 		}
-
 	}
+
 	return (-1);
 }
