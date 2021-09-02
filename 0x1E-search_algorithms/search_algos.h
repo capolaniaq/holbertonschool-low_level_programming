38de1142b43251ca07021150ad5ea807e568d06c
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-09-02 10:16:52 -0500

    104-advanced_binary.c

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -20,4 +20,6 @@ int binarySearch(int *arr, size_t l, size_t r, int x);
 /**/
 int jump_search(int *array, size_t size, int value);
 
+int advanced_binary(int *array, size_t size, int value);
+
 #endif
